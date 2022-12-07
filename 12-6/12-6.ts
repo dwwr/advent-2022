c397ff3ce9a34fc26d5f43b95e41da6e5ae6626b
dwwr <dwwarnerreyes@gmail.com>
2022-12-06 19:27:41 -0800

    12-6 part 1

--- 12-6/12-6.ts
+++ 12-6/12-6.ts
@@ -1,8 +1,18 @@
 import { input } from './input'
 
+
+// this is all bad
 const detectStartOfPacketMarker = (input: string): number => {
-  console.log(input)
-  return 1
+  const markerDepth = [...input].reduce((depth, c, i) => {
+    const slice = `${input[depth]}${input[depth + 1]}${input[depth + 2]}${
+      input[depth + 3]
+    }`
+    if (slice.match(/^.*(.).*\1.*$/) === null) {
+      return depth
+    }
+    return depth + 1
+  }, 0)
+  return markerDepth + 4 // offset
 }
 
-detectStartOfPacketMarker(input)
+console.log(detectStartOfPacketMarker(input))

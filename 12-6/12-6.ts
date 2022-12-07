c98b36dfa331022a75057959a360b95e0dc3e97b
dwwr <dwwarnerreyes@gmail.com>
2022-12-06 19:47:51 -0800

    12-6 naive solution

--- 12-6/12-6.ts
+++ 12-6/12-6.ts
@@ -2,15 +2,19 @@ import { input } from './input'
 
 
 // this is all bad
-const detectStartOfPacketMarker = (input: string): number => {
+const detectStartOfPacketMarker = (
+  input: string,
+  markerLength: number
+): number => {
   const markerDepth = [...input].reduce((depth) => {
-    const slice = input.slice(depth, depth + 4)
+    const slice = input.slice(depth, depth + markerLength)
     if (slice.match(/^.*(.).*\1.*$/) === null) {
       return depth
     }
     return depth + 1
   }, 0)
-  return markerDepth + 4 // offset
+  return markerDepth + markerLength // offset
 }
 
-console.log(detectStartOfPacketMarker(input))
+console.log(detectStartOfPacketMarker(input, 4))
+console.log(detectStartOfPacketMarker(input, 14))

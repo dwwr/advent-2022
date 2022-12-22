350c60fa53cb173eeeb2dd49b8a648b1965445d8
dwwr <derek.warnerreyes@olioapps.com>
2022-12-21 23:00:36 -0800

    still trying

--- 12-7/12-7.ts
+++ 12-7/12-7.ts
@@ -10,18 +10,18 @@ $ ls
 dir e
 29116 f
 2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k`
+62596 h.lst`
+// $ cd e
+// $ ls
+// 584 i
+// $ cd ..
+// $ cd ..
+// $ cd d
+// $ ls
+// 4060174 j
+// 8033020 d.log
+// 5626152 d.ext
+// 7214296 k`
 
 // build tree from terminal output
 // get sums of dirs (recursively)
@@ -31,7 +31,16 @@ interface Node {
   name: string
   size?: number
   parent?: Node
-  children: Node[]
+  children?: Record<string, any>
+  lastDir?: string
+}
+
+const makeNode = (name: string, lastDir?: string): Node => {
+  return {
+    name,
+    lastDir,
+    size: 0,
+  }
 }
 
 const buildFS = (input: string): Node => {
@@ -39,11 +48,31 @@ const buildFS = (input: string): Node => {
 
   const tree = commands.reduce(
     (a: Node, c) => {
+      const args = c.split(' ')
+      if (args[1] === 'cd') {
+        if (args[2] === '..') {
+          return {
+            ...a,
+            children: {
+              ...a.children,
+              [a.lastDir!]: buildNode(c, makeNode(c, a.lastDir)),
+            },
+          }
+        }
+        return {
+          ...a,
+          lastDir: a.name,
+          children: {
+            ...a.children,
+            [args[3]]: buildNode(c, makeNode(c, a.name)),
+          },
+        }
+      }
       return buildNode(c, a)
     },
     {
       name: '/',
-      children: [],
+      children: {},
     }
   )
   console.log('tree', JSON.stringify(tree))
@@ -54,25 +83,35 @@ const buildNode = (input: string, tree: Node): Node => {
   const args = input.split(' ')
   // console.log('command', input)
   console.log(args)
-
   console.log(tree)
-  if (args[1] === 'cd') {
-    if (args[2] === '..') {
-      return tree
-    }
+  if (!!Number(args[0])) {
     return {
       ...tree,
-      children: [...tree.children, buildNode(input, tree)],
+      children: {
+        ...tree.children,
+
+        [input]: { name: input, size: Number(args[0]) },
+      },
     }
   }
+  // if (args[1] === 'cd') {
+  //   if (args[2] === '..') {
+  //     return { ...tree }
+  //   }
+  //   return { ...tree }
+  // }
+  if (args[0] === 'dir') {
+    return {
+      ...tree,
+      children: {
+        ...tree.children,
 
-  if (args[1] === 'ls') {
-    return tree
+        [input]: { name: input, children: {} },
+      },
+    }
   }
-
   return {
     ...tree,
-    children: [...tree.children, { name: input, children: [] }],
   }
 }
 
